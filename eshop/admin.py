d63c8c01278a88dde170d8316bf2df178a5bb6ed
Nikita Rozkov <nikita.rozkov1994@gmail.com>
2021-01-28 22:27:20 -0800

    make migration add products

--- eshop/admin.py
+++ eshop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Category)
+admin.site.register(Hoodies, HoodieAdmin)
+admin.site.register(Tshirt)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
--- eshop/models.py
+++ eshop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
@@ -9,7 +10,6 @@ User = get_user_model()
 #3 Add CartProduct
 #4 Cart
 #5 Order
-#6 Specifications
 
 class Category(models.Model):
 
@@ -21,20 +21,46 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    class Meta:
+        abstract = True
+
     title = models.CharField(max_length=255, verbose_name="Product name")
     slug = models.SlugField(unique=True)
     image = models.ImageField()
-    description = models.TextField(verbose_name="description", null=Ture)
+    description = models.TextField(verbose_name="description", null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="price")
 
     def __str__(self):
         return self.title
 
+class Hoodies(Product):
+
+    size = models.CharField(max_length=255,verbose_name="size")
+    color = models.CharField(max_length=255, verbose_name="color")
+    style = models.CharField(max_length=255, verbose_name="style")
+    firm = models.CharField(max_length=255, verbose_name="firm")
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
+class Tshirt(Product):
+
+    size = models.CharField(max_length=255, verbose_name="size")
+    color = models.CharField(max_length=255, verbose_name="color")
+    style = models.CharField(max_length=255, verbose_name="style")
+    firm = models.CharField(max_length=255, verbose_name="firm")
+
+    def __str__(self):
+        return "{} : {}".format(self.category_name, self.title)
+
+
 class CartProduct(models.Model):
 
     user = models.ForeignKey("Customer",verbose_name='Buyers', on_delete=models.CASCADE)
-    cart = models.ForeignKey("Cart", verbose_name="Cart", on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
+    cart = models.ForeignKey("Cart", verbose_name="Cart", on_delete=models.CASCADE, related_name="related_products")
+    contet_type = models.ForeignKey(ContentType, on_delete = models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    #content_object = GenericForeignKey("content_type","object_id")
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Total amaut")
 
@@ -44,7 +70,7 @@ class CartProduct(models.Model):
 class Cart(models.Model):
 
     owner = models.ForeignKey("Customer", verbose_name="Owner", on_delete=models.CASCADE)
-    products = models.ManyToManyField(CartProduct, blank=True)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name="related_cart")
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9,decimal_places=2, verbose_name="Total amaut")
 
@@ -61,10 +87,3 @@ class Customer(models.Model):
         return "Buyer: {} {}".format(self.user.first_name, self.user.last_name)
 
 
-class Specifications(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name="Name of Product for Specifications")
-
-    def __str__(self):
-        return "Specifications for product: {}".format(self.name)
