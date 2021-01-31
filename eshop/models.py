dea575159cd43d060740f1ac54323656eaf9d987
ChesGol <goloburda.c@gmail.com>
2021-01-31 11:05:44 +0200

    modeliu klaidu taisymas

--- eshop/models.py
+++ eshop/models.py
@@ -5,11 +5,6 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
-#1 Add Category
-#2 Add Product
-#3 Add CartProduct
-#4 Cart
-#5 Order
 
 class Category(models.Model):
 
@@ -18,6 +13,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
 
@@ -33,17 +29,19 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+
 class Notebook(Product):
 
-    diagonal = models.CharField(max_length=255,verbose_name="Diagonal")
+    diagonal = models.CharField(max_length=255, verbose_name="Diagonal")
     display_type = models.CharField(max_length=255, verbose_name="Display type")
-    procesor_freq = models.CharField(max_length=255, verbose_name="Processor freq.")
+    processor_freq = models.CharField(max_length=255, verbose_name="Processor freq.")
     ram = models.CharField(max_length=255, verbose_name="Ram")
     video = models.CharField(max_length=255, verbose_name="Video")
     time_without_charge = models.CharField(max_length=255, verbose_name="Time without charge")
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
 
 class Smartphone(Product):
 
@@ -63,32 +61,34 @@ class Smartphone(Product):
 
 class CartProduct(models.Model):
 
-    user = models.ForeignKey("Customer",verbose_name='Buyers', on_delete=models.CASCADE)
+    user = models.ForeignKey("Customer", verbose_name='Buyers', on_delete=models.CASCADE)
     cart = models.ForeignKey("Cart", verbose_name="Cart", on_delete=models.CASCADE, related_name="related_products")
-    contet_type = models.ForeignKey(ContentType, on_delete = models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    #content_object = GenericForeignKey("content_type","object_id")
+    content_object = GenericForeignKey("content_type", "object_id")
     qty = models.PositiveIntegerField(default=1)
-    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Total amaut")
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Total amount")
 
     def __str__(self):
         return "Product {} (for cart)".format(self.product.title)
+
 
 class Cart(models.Model):
 
     owner = models.ForeignKey("Customer", verbose_name="Owner", on_delete=models.CASCADE)
     products = models.ManyToManyField(CartProduct, blank=True, related_name="related_cart")
     total_products = models.PositiveIntegerField(default=0)
-    final_price = models.DecimalField(max_digits=9,decimal_places=2, verbose_name="Total amaut")
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Total amount")
 
     def __str__(self):
         return str(self.id)
+
 
 class Customer(models.Model):
 
     user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name="Telephone number")
-    adress = models.CharField(max_length=255, verbose_name="Adress")
+    address = models.CharField(max_length=255, verbose_name="Address")
 
     def __str__(self):
         return "Buyer: {} {}".format(self.user.first_name, self.user.last_name)
