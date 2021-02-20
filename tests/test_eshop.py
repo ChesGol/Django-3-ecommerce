000b7234fdf38efe8e5770063f8e0d24e390d940
Nikita <nikita.rozkov1994@gmail.com>
2021-02-20 10:26:43 +0200

    testing

--- tests/test_eshop.py
+++ tests/test_eshop.py
@@ -1,35 +1,35 @@
 from decimal import Decimal
 from unittest import mock
 
-from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import TestCase, RequestFactory, client
 from django.contrib.auth import get_user_model
-
-from eshop.models import Category, CartProduct, Cart, Customer, Product
-from eshop.views import recalc_cart, AddToCartView, BaseView
+from django.test import TestCase, RequestFactory, client
+from django.core.files.uploadedfile import SimpleUploadedFile
+from eshop.models import Product, Category, Cart, CartProduct, Customer
+from eshop.utils import recalc_cart
+from eshop.views import AddToCartView, BaseView
 
 User = get_user_model()
 
 
-class ShopTestCases(TestCase):
-
-    def setUp(self) -> None:
-        self.user = User.objects.create(username='tester', password='password')
-        self.category = Category.objects.create(name='Notebooks', slug='notebooks')
+class ProductModelTest(TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.user = User.objects.create(username='tester', password='password')
+        cls.category = Category.objects.create(name='Notebooks', slug='notebooks')
         image = SimpleUploadedFile("notebook_image.jpg", content=b'', content_type="image/jpg")
-        self.notebook = Product.objects.create(
-            category=self.category,
-            title="Test Notebook",
-            slug="test-slug",
+        product = Product.objects.create(
+            category=cls.category,
+            title='something',
+            slug='test-slug',
             image=image,
-            price=Decimal('5000.00')
+            description='about notebook',
+            price=500,
         )
-        self.customer = Customer.objects.create(user=self.user, phone="222222211", address="Address")
-        self.cart = Cart.objects.create(owner=self.customer)
-        self.cart_product = CartProduct.objects.create(
-            user=self.customer,
-            cart=self.cart,
-            content_objects=self.notebook
+        cls.customer = Customer.objects.create(user=cls.user, phone="222222211", address="Address")
+        cls.cart = Cart.objects.create(owner=cls.customer)
+        cls.cart_product = CartProduct.objects.create(
+            user=cls.customer,
+            cart=cls.cart,
         )
 
     def test_add_to_cart(self):
@@ -57,3 +57,20 @@ class ShopTestCases(TestCase):
             request.user = self.user
             response = BaseView.as_view()(request)
             self.assertEqual(response.status_code, 444)
+
+
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get(':8000/')
+        self.assertEqual(response.status_code, 200)
+
+     def test_version(self):
+         assert self.get('/').status_code == 200
+
+
+
+    def test_title_max_length(self):
+        product = Product.objects.get()
+        max_length = product._meta.get_field('title').max_length
+        self.assertEqual(max_length, 255)
